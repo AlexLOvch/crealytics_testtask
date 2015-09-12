4a28239162c1c127b9b4f9e2fe32ce1781631b97
AlexLOvch <alex.l.ovchinnikov@gmail.com>
2015-09-12 16:01:22 +0300

    extract merger class

--- lib/modifier.rb
+++ lib/modifier.rb
@@ -14,16 +14,62 @@ class Float
   end
 end
 
-class Modifier
-  LINES_PER_FILE = 120000
-  DEFAULT_CSV_OPTIONS = { :col_sep => "\t", :headers => :first_row }
 
+
+class Merger
+  def initialize(enumerator, fields_for_last, fields_for_last_real)
+    @enumerator = enumerator
+    @fields_for_last = fields_for_last
+    @fields_for_last_real = fields_for_last_real
+  end
+
+  def merge
+    Enumerator.new do |yielder|
+      while true
+        begin
+          list_of_rows = @enumerator.next
+          merged = merge_hashes(list_of_rows.map{|r|r.nil? ? nil : r.headers}, list_of_rows.map{|r|r.nil? ? nil : r.fields})
+          yielder.yield(merged)
+        rescue StopIteration
+          break
+        end
+      end
+    end
+  end
+
+  def merge_values(key, value_arr)
+    if @fields_for_last.include?(key)
+      value_arr.last
+    elsif @fields_for_last_real.include?(key)
+      value_arr.reject{|v| [0, '0', nil, ''].include?(v) }.last
+    else
+      value_arr[0]
+    end
+  end
+
+
+  def merge_hashes(headers, list_of_datas)
+    keys = headers.map{|h| h.nil? ? nil :  h}.compact.flatten.uniq
+    result = {}
+    keys.each do |key|
+      result[key] = []
+      list_of_datas.each_with_index do |data, i|
+        result[key] << (data.nil? ? nil : data[headers[i].index(key)])
+      end
+      result[key] = merge_values(key, result[key])
+    end
+    result
+  end
+end
+
+
+class Modifier
   KEYWORD_UNIQUE_ID = 'Keyword Unique ID'
   LAST_VALUE_WINS = ['Account ID', 'Account Name', 'Campaign', 'Ad Group', 'Keyword', 'Keyword Type', 'Subid', 'Paused', 'Max CPC', 'Keyword Unique ID', 'ACCOUNT', 'CAMPAIGN', 'BRAND', 'BRAND+CATEGORY', 'ADGROUP', 'KEYWORD']
   LAST_REAL_VALUE_WINS = ['Last Avg CPC', 'Last Avg Pos']
   INT_VALUES = ['Clicks', 'Impressions', 'ACCOUNT - Clicks', 'CAMPAIGN - Clicks', 'BRAND - Clicks', 'BRAND+CATEGORY - Clicks', 'ADGROUP - Clicks', 'KEYWORD - Clicks']
   FLOAT_VALUES = ['Avg CPC', 'CTR', 'Est EPC', 'newBid', 'Costs', 'Avg Pos']
-  COMMISSION_VALUES =['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value']
+  COMMISSION_VALUES = ['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value']
   NUMBER_OF_COMMISSIONS = ['number of commissions']
 
 
@@ -34,12 +80,13 @@ class Modifier
   end
 
   def modify
+    merger = Merger.new(@enumerator, LAST_VALUE_WINS, LAST_REAL_VALUE_WINS).merge
+
     Enumerator.new do |yielder|
       while true
         begin
-          list_of_rows = @enumerator.next
-          merged = combine_hashes(list_of_rows.map{|r|r.nil? ? nil : r.headers}, list_of_rows.map{|r|r.nil? ? nil : r.fields})
-          yielder.yield(combine_values(merged))
+          merged = merger.next
+          yielder.yield(modify_values(merged))
         rescue StopIteration
           break
         end
@@ -49,60 +96,19 @@ class Modifier
 
   private
 
-=begin
-  def combine(merged)
-    result = []
-    merged.each do |_, hash|
-      result << combine_values(hash)
-    end
-    result
-  end
-=end
-
-  def combine_values(hash)
-    LAST_VALUE_WINS.each do |key|
-      hash[key] = hash[key].last
-    end
-    LAST_REAL_VALUE_WINS.each do |key|
-      hash[key] = hash[key].select {|v| not (v.nil? or v == 0 or v == '0' or v == '')}.last
-    end
+  def modify_values(hash)
     INT_VALUES.each do |key|
-      hash[key] = hash[key][0].to_s
+      hash[key] = hash[key].to_s
     end
     FLOAT_VALUES.each do |key|
-      hash[key] = hash[key][0].from_german_to_f.to_german_s
+      hash[key] = hash[key].from_german_to_f.to_german_s
     end
     NUMBER_OF_COMMISSIONS.each do |key|
-      hash[key] = (@cancellation_factor * hash[key][0].from_german_to_f).to_german_s
+      hash[key] = (@cancellation_factor * hash[key].from_german_to_f).to_german_s
     end
     COMMISSION_VALUES.each do |key|
-      hash[key] = (@cancellation_factor * @saleamount_factor * hash[key][0].from_german_to_f).to_german_s
+      hash[key] = (@cancellation_factor * @saleamount_factor * hash[key].from_german_to_f).to_german_s
     end
-    p ""
-    p "combine_values(hash): result: #{hash}"
-    p ""
     hash
   end
-
-  def combine_hashes(headers, list_of_data)
-    keys = []
-    headers.each do |header|
-      next if header.nil?
-      header.each do |key|
-        keys << key
-      end
-    end
-    result = {}
-    keys.each do |key|
-      result[key] = []
-      list_of_data.each_with_index do |data, i|
-        result[key] << (data.nil? ? nil : data[headers[i].index(key)])
-      end
-    end
-    p ""
-    p "combine_hashes(headers, list_of_data) result: #{result}"
-    p ""
-    result
-  end
-
 end
